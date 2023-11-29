98b79ffdbc609fabfcbcfc3eef569427b40581e0
Carmen <carmenshiu07@gmail.com>
2023-11-29 14:52:05 -0800

    edited error handler in app.js

--- backend/app.js
+++ backend/app.js
@@ -64,12 +64,22 @@ app.use((err, _req, _res, next) => {
 app.use((err, _req, res, _next) => {
     res.status(err.status || 500);
     console.error(err);
-    res.json({
-        // title: err.title || 'Server Error',
+
+    const response = {
         message: err.message,
-        errors: err.errors,
-        // stack: isProduction ? null : err.stack
-    });
+        error: err.errors,
+    };
+
+    if (!isProduction) response.stack = err.stack;
+
+    res.json(response);
+
+    // res.json({
+    // title: err.title || 'Server Error',
+    // message: err.message,
+    // errors: err.errors,
+    // stack: isProduction ? null : err.stack
+    // });
 });
 
 module.exports = app;
