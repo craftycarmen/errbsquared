674bc3c1cf1e613e822651be3ec77741c457ec8d
Carmen <carmenshiu07@gmail.com>
2023-11-30 15:34:11 -0800

    successfully added get details owned by a current user

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -3,6 +3,8 @@ const { Spot, Review, Image } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+
 const router = express.Router();
 
 router.get('/', async (req, res) => {
@@ -42,21 +44,63 @@ router.get('/', async (req, res) => {
         delete spot.Reviews;
     });
 
-
-
-    // spotsList.forEach(spot => {
-    //     images.forEach(image => {
-    //         if (image.imageableType === 'Spot' && image.imageableId === spot.id && image.previewImage === true) {
-    //             spot.previewImage = image.url
-    //         }
-    //     });
-
-    //     delete spot.Images;
-    // });
-
-
-
     return res.json({ Spots: spotsList })
+});
+
+router.get('/current', async (req, res) => {
+    const { user } = req;
+
+    if (user) {
+        const safeUser = {
+            id: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            username: user.username
+        };
+
+        const spots = await Spot.findAll({
+            include: [{
+                model: Review
+            }],
+            where: {
+                ownerId: user.id
+            }
+        })
+
+        let spotsList = [];
+
+        spots.forEach(spot => {
+            spotsList.push(spot.toJSON());
+        });
+
+        const images = await Image.findAll({
+            include: [Spot]
+        })
+
+        spotsList.forEach(spot => {
+            spot.Reviews.forEach(review => {
+                if (review.stars) {
+                    let totalStars = spot.Reviews.reduce((sum, review) => (sum + review.stars), 0)
+                    avgStars = totalStars / spot.Reviews.length
+                    spot.avgRating = avgStars;
+                }
+            });
+
+            images.forEach(image => {
+                if (image.imageableType === 'Spot' && image.imageableId === spot.id && image.previewImage === true) {
+                    spot.previewImage = image.url
+                }
+            });
+
+            delete spot.Images;
+            delete spot.Reviews;
+        });
+
+        return res.json({
+            Spots: spotsList
+        });
+    } else return res.json({ user: null })
 })
 
 module.exports = router;
