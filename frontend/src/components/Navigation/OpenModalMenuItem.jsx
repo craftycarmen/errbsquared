1d52487092084831e1901c5a9d02c1a12d920442
Carmen <carmenshiu07@gmail.com>
2024-01-12 16:23:34 -0800

    refactored dropdown menu and added OpenModalMenuItem

---
+++ frontend/src/components/Navigation/OpenModalMenuItem.jsx
@@ -0,0 +1,17 @@
+import { useModal } from "../../context/Modal";
+
+export default function OpenModalMenuItem({
+    modalComponent, itemText, onItemClick, onModalClose }) {
+
+    const { setModalContent, setOnModalClose } = useModal();
+
+    const onClick = () => {
+        if (onModalClose) setOnModalClose(onModalClose);
+        setModalContent(modalComponent);
+        if (typeof onItemClick === "function") onItemClick();
+    };
+
+    return (
+        <li onClick={onClick}>{itemText}</li>
+    );
+}
