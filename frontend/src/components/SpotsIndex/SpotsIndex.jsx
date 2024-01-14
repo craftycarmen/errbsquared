c2f0429bfe82012df82cb504dcea35f68c25f6ec
Carmen <carmenshiu07@gmail.com>
2024-01-13 16:12:29 -0800

    added title and link to SpotsIndex

--- frontend/src/components/SpotsIndex/SpotsIndex.jsx
+++ frontend/src/components/SpotsIndex/SpotsIndex.jsx
@@ -2,10 +2,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import './SpotsIndex.css';
 import { getAllSpots, spotsArr } from '../../store/spots';
 import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function SpotsIndex() {
     const dispatch = useDispatch();
     const allSpots = useSelector(spotsArr);
+    console.log(allSpots);
 
     useEffect(() => {
         dispatch(getAllSpots())
@@ -18,16 +20,19 @@ export default function SpotsIndex() {
                 {
                     allSpots.map((spot) => (
                         <div key={spot.id} className='spotCard'>
-                            <img
-                                src={spot.previewImage}
-                            />
-                            <p>{spot.city}, {spot.state}</p>
-                            <p>${spot.price} / night</p>
-                            <p><i className="fa-solid fa-star" /> {spot.avgRating}</p>
+                            <Link to={`spot/${spot.id}`}>
+                                <img
+                                    title={spot.name}
+                                    src={spot.previewImage}
+                                />
+                                <p>{spot.city}, {spot.state}</p>
+                                <p>${spot.price} / night</p>
+                                <p><i className="fa-solid fa-star" /> {spot.avgRating}</p>
+                            </Link>
                         </div>
                     ))
                 }
             </div>
-        </section>
+        </section >
     )
 }
