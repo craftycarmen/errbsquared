8e044db1637497245477dfa0fc24849aac86149a
Carmen <carmenshiu07@gmail.com>
2024-01-15 00:37:46 -0800

    added spot && to return

--- frontend/src/components/SpotDetails/SpotDetails.jsx
+++ frontend/src/components/SpotDetails/SpotDetails.jsx
@@ -7,39 +7,38 @@ import './SpotDetails.css';
 export default function SpotDetails() {
     const { spotId } = useParams();
     const dispatch = useDispatch();
-    const spot = useSelector(state => state.spots ? state.spots[spotId] : null
-    );
+    const spot = useSelector(state => state.spots ? state.spots[spotId] : null);
 
     useEffect(() => {
         dispatch(fetchSpotDetails(spotId));
     }, [dispatch, spotId]);
 
-    return (
+    return (spot &&
         <section className='container'>
             <div className='header'>
-                <h1>{spot?.name}</h1>
-                <div>{spot?.city}, {spot?.state}, {spot?.country}</div>
+                <h1>{spot.name}</h1>
+                <div>{spot.city}, {spot.state}, {spot.country}</div>
             </div>
             <div className='gallery galleryContainer'>
-                {spot?.SpotImages?.map(image => (
+                {spot.SpotImages?.map(image => (
                     <img
-                        key={image?.id}
-                        src={image?.url}
+                        key={image.id}
+                        src={image.url}
                     />
                 ))}
             </div>
             <div className='details'>
-                <h2>Hosted by {spot?.Owner?.firstName} {spot?.Owner?.lastName}</h2>
-                <div>{spot?.description}</div>
+                <h2>Hosted by {spot.Owner?.firstName} {spot.Owner?.lastName}</h2>
+                <div>{spot.description}</div>
             </div>
             <div className='box'>
-                <div>${spot?.price} / night</div>
-                <p><i className="fa-solid fa-star" /> {spot?.avgStarRating} · {spot?.numReviews} reviews</p>
+                <div>${spot.price} / night</div>
+                <p><i className="fa-solid fa-star" /> {spot.avgStarRating} · {spot?.numReviews} reviews</p>
                 <button onClick={() => alert('Feature coming soon')}>Reserve</button>
             </div>
             <div className='reviews'>
                 <hr />
-                <h3><i className="fa-solid fa-star" /> {spot?.avgStarRating} · {spot?.numReviews} reviews</h3>
+                <h3><i className="fa-solid fa-star" /> {spot.avgStarRating} · {spot.numReviews} reviews</h3>
             </div>
         </section>
     )
