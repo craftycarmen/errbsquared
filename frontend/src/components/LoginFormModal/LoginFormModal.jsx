04dae07f82914776b2089648df9efa12f599400a
Carmen <carmenshiu07@gmail.com>
2024-01-16 17:42:09 -0800

    added demo user

--- frontend/src/components/LoginFormModal/LoginFormModal.jsx
+++ frontend/src/components/LoginFormModal/LoginFormModal.jsx
@@ -1,15 +1,24 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import * as sessionActions from '../../store/session';
 import { useDispatch } from 'react-redux';
 import { useModal } from '../../context/Modal';
 import './LoginForm.css';
+import { Link } from "react-router-dom";
 
 export default function LoginFormModal() {
     const dispatch = useDispatch();
     const [credential, setCredential] = useState("");
     const [password, setPassword] = useState("");
+    const [charCount, setCharCount] = useState({});
     const [errors, setErrors] = useState({});
     const { closeModal } = useModal();
+
+    useEffect(() => {
+        const char = {}
+        if (credential.length < 4) char.credential = 'Not enough characters'
+        if (password.length < 6) char.password = 'Not enough characters'
+        setCharCount(char)
+    }, [credential, password])
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -31,33 +40,60 @@ export default function LoginFormModal() {
             });
     };
 
+    const demoUser = () => {
+        return dispatch(
+            sessionActions.login({
+                credential: "Demo-lition",
+                password: "password"
+            })
+        )
+            .then(closeModal)
+            .catch(async (res) => {
+                const data = await res.json();
+
+                if (data?.errors) {
+                    setErrors(data.errors);
+                }
+            });
+    };
+
     return (
-        <>
+        <section className="login">
             <h1>Log In</h1>
+            <p className="error">
+                {errors.credential && `${errors.credential}`}
+            </p>
             <form onSubmit={handleSubmit}>
+                <div>
+                    <label>
+                        <input
+                            placeholder="Username or Email"
+                            type="text"
+                            value={credential}
+                            onChange={(e) => setCredential(e.target.value)}
+                            required
+                        />
+                    </label>
+                </div>
                 <label>
-                    Username or Email
                     <input
-                        type="text"
-                        value={credential}
-                        onChange={(e) => setCredential(e.target.value)}
-                        required
-                    />
-                </label>
-                <label>
-                    Password
-                    <input
+                        placeholder="Password"
                         type="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         required
                     />
                 </label>
-                <div className="error">
-                    {errors.credential && `* ${errors.credential}`}
+                <div>
+                    <button
+                        disabled={Object.values(charCount).length}
+                        type="submit">Log In</button>
                 </div>
-                <button type="submit">Log In</button>
+                <div>
+                    <button onClick={demoUser}>Demo User</button>
+                </div>
             </form>
-        </>
+
+        </section >
     );
 }
