eb099b099be250f8fe107a953921a49a7de45271
Carmen <carmenshiu07@gmail.com>
2024-01-21 22:05:35 -0800

    css cleanup

--- frontend/src/components/CreateReviewModal/CreateReviewButton.jsx
+++ frontend/src/components/CreateReviewModal/CreateReviewButton.jsx
@@ -1,11 +1,11 @@
 import OpenModalButton from "../OpenModalButton/OpenModalButton";
 import CreateReviewModal from "./CreateReviewModal";
 
-export default function CreateReviewButton({ spotId, sessionUser }) {
+export default function CreateReviewButton({ spotId }) {
     return (
         <OpenModalButton
             buttonText="Post Your Review"
-            modalComponent={<CreateReviewModal spotId={spotId} sessionUser={sessionUser} />}
+            modalComponent={<CreateReviewModal spotId={spotId} />}
         />
     );
 }
